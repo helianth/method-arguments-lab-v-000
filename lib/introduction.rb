25e6bb9a8e26ee817437bf20e0a248a3c6d437a9
Lamia Haider <lamia.haider@gmail.com>
2018-04-01 20:49:15 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,2 @@
-def introduction(name = "Dan", language = "Ember.js" )
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
+def introduction(name, language)
+  "Hi, my name is #{name}."
